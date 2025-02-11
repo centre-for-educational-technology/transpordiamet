460a465d69bc6c40534d706418a0a0de97adb7c7
Annabel Väljaots <114983045+annabelv@users.noreply.github.com>
2025-02-11 15:44:44 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Road from './road';
+
+//Adding the road.tsx div
+const App: React.FC = () => {
+    return (
+        <div>
+            <Road /> 
+        </div>
+    );
+};
+
+export default App
---
+++ src/road.tsx
@@ -0,0 +1,8 @@
+import './road.css'
+
+//Creating a div to draw a road on the screen
+const Road: React.FC = () => {
+    return <div className="road"></div>; // Use the class name
+};
+  
+export default Road;
